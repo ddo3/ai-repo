2d218a10ff8487fcbc542cc55f616d2cb4942569
ddo3 <ddo3@case.edu>
2018-11-23 22:57:58 -0500

    added k-means code - not tested

--- aihw5/hw5-graph.py
+++ aihw5/hw5-graph.py
@@ -230,125 +230,48 @@ def hw5_p2_parta():#DONE
     make_plots_for_p2partA(less_than_half_cherry, 'less_than_half_cherry')
 
 ####################################################################################################################################
-def make_ten_candy_lists(lime: int, cherry: int) -> dict:
-    candy_lists = {}
 
-    for i in range(0,10):
-        candy_lists[i] = get_candy_list(lime, cherry)
-
-    return candy_lists
-
-#todo - instead of doing this, plot the difference of the values for the average ????
-#then only do one graph???
-def ave_values_at_index(index: int, hypothesis_num: int, list_of_data: List) -> float:
-    divisor = len(list_of_data)
-
-    sum = 0
-
-    for t in list_of_data:
-        hypoth_list = t[hypothesis_num -1]
-        sum = sum + hypoth_list[index]
-
-    return sum/divisor
-
-
-def get_posterior_for_candy_lists(candy_lists: dict, num: int) -> Tuple:
-    list_of_data = []
-    #print(candy_lists[])
-
-    for j in range(0, num):
-
-        data1 = posterior_prob_of_hypothesis(1, candy_lists[j])
-        data2 = posterior_prob_of_hypothesis(2, candy_lists[j])
-        data3 = posterior_prob_of_hypothesis(3, candy_lists[j])
-        data4 = posterior_prob_of_hypothesis(4, candy_lists[j])
-        data5 = posterior_prob_of_hypothesis(5, candy_lists[j])
-
-        for i in range(0 , len(data1)):
-            d1val = data1[i]
-            d2val = data2[i]
-            d3val = data3[i]
-            d4val = data4[i]
-            d5val = data5[i]
-
-            sum = d1val + d2val + d3val + d4val + d5val
-
-            data1[i] = d1val/sum
-            data2[i] = d2val/sum
-            data3[i] = d3val/sum
-            data4[i] = d4val/sum
-            data5[i] = d5val/sum
-
-        list_of_data.append( (data1, data2, data3, data4, data5) )
-
-    #now we have the normal data for just one graph
-    #we need to do this for every candy list in the main List
-    #and then average the results
-
-    if(num == 1):
-        return list_of_data[0]
-    else:
-        final_data1 = []
-        final_data2 = []
-        final_data3 = []
-        final_data4 = []
-        final_data5 = []
-
-        example = list_of_data[0]
-        ex2 = example[0]
-        #print(typeof(example))
-
-        for j in range(0, len(example[0])):
-            final_data1.append(ave_values_at_index(j, 1, list_of_data))
-            final_data2.append(ave_values_at_index(j, 2, list_of_data))
-            final_data3.append(ave_values_at_index(j, 3, list_of_data))
-            final_data4.append(ave_values_at_index(j, 4, list_of_data))
-            final_data5.append(ave_values_at_index(j, 5, list_of_data))
-
-        return (final_data1, final_data2, final_data3, final_data4, final_data5)
-
-
-def make_plots_for_p2partC(candy_lists: dict, hypothesis_num: int, title: str) -> None:
+def make_plots_for_p2partC(candy_lists: tuple) -> None:
 
     x = range(0, len(candy_lists[0]))
 
     plt.figure(1)
 
+    hypothesis = 1
+    while (hypothesis != 6):
 
-    data_tuple = get_posterior_for_candy_lists(candy_lists, hypothesis_num)
-    plt.subplot(1, 3, 1)
-    plt.title("One Data Set")
-    plt.plot(x, data_tuple[0],'r',x,data_tuple[1],'b',x,data_tuple[2],'g',x,data_tuple[3],'m',x,data_tuple[4],'k')
+        data1 = posterior_prob_of_hypothesis(hypothesis, candy_lists[0])
+        data2 = posterior_prob_of_hypothesis(hypothesis, candy_lists[1])
+        data3 = posterior_prob_of_hypothesis(hypothesis, candy_lists[2])
+        data4 = posterior_prob_of_hypothesis(hypothesis, candy_lists[3])
+        data5 = posterior_prob_of_hypothesis(hypothesis, candy_lists[4])
+
+        final_list = []
+
+        for i in range(0, len(data1)):
+            sum = data1[i] + data2[i] + data3[i] + data4[i] + data5[i]
+            final_list.append( sum/5 )
 
 
-    data_tuple5 = get_posterior_for_candy_lists(candy_lists, 5)
-    plt.subplot(1, 3, 2)
-    plt.title("Five Data Sets")
-    plt.plot(x, data_tuple5[0],'r',x,data_tuple5[1],'b',x,data_tuple5[2],'g',x,data_tuple5[3],'m',x,data_tuple5[4],'k')
+        plt.plot(x, final_list)
 
+        hypothesis = hypothesis + 1
 
-    data_tuple10 = get_posterior_for_candy_lists(candy_lists, 10)
-    plt.subplot(1, 3, 3)
-    plt.title("Ten Data Sets")
-    plt.plot(x, data_tuple10[0],'r',x,data_tuple10[1],'b',x,data_tuple10[2],'g',x,data_tuple10[3],'m',x,data_tuple10[4],'k')
-
-    plt.suptitle(title)
+    plt.gca().legend(('h1','h2', 'h3', 'h4', 'h5'))
     plt.show()
 
 # ai hw 5 problem 2 part c
 def hw5_p2_partc() -> None:
 
-    all_cherry_lists = make_ten_candy_lists(0, 100)
-    most_cherry_lists = make_ten_candy_lists(25,75)
-    half_cherry_lists = make_ten_candy_lists(50,50)
-    less_cherry_lists = make_ten_candy_lists(75, 25)
-    all_lime_lists = make_ten_candy_lists(100,0)
+    all_cherry_list = get_candy_list(0, 100)
+    most_cherry_list = get_candy_list(25, 75)
+    half_cherry_list = get_candy_list(50, 50)
+    less_cherry_list = get_candy_list(75, 25)
+    all_lime_list = get_candy_list(100, 0)
 
-    make_plots_for_p2partC(all_cherry_lists, 1, "All Cherry Candies")
-    make_plots_for_p2partC(most_cherry_lists, 2, "75% Cherry Candies")
-    make_plots_for_p2partC(half_cherry_lists, 3, "Half and Half")
-    make_plots_for_p2partC(less_cherry_lists, 4, "25% Cherry Candies")
-    make_plots_for_p2partC(all_lime_lists, 5, "All Lime Candies")
+    candy_lists = (all_cherry_list, most_cherry_list, half_cherry_list, less_cherry_list, all_lime_list)
+
+    make_plots_for_p2partC(candy_lists)
 
 #################################################################################################
 sepal_length = []
@@ -391,12 +314,46 @@ def plot_cluster(cluster_x: List, cluster_y: List, cluster_assign: List, title: 
     plt.plot(cluster_x, cluster_y, marker = "*")
 
     plt.show()
-    pass()
 
 def distance(x1: float, y1: float, x2: float, y2: float) -> float:
     dist = np.sqrt( np.power((x2 - x1), 2) + np.power((y2 - y1), 2) )
     return np.absolute(dist)
 
+def plot_with_mean_points(mean_x: list, mean_y: list, title:str) -> None:
+
+    #plot the data
+    plt.plot(petal_length, petal_width, 'o')
+
+    plt.plot(mean_x, mean_y, '*')
+
+    plt.title(title)
+
+    plt.show()
+
+
+def check_if_need_to_loop(cluster_assign_1: list, cluster_assign_2: list) -> bool:
+    for i in range(0, len(cluster_assign_1)):
+        if(cluster_assign_1[i] != cluster_assign_2[i]):
+            return False
+
+    return True
+
+def update_cluster_mean( mean_list: list, data_list: list, cluster_assign_list: list) -> List:
+
+    new_mean_list = []
+
+    for i in range(0, len(mean_list)):
+        counter = 0
+        sum = 0
+
+        for j in range(0, len(cluster_assign_list)):
+            if(cluster_assign_list[j] == i):
+                sum = sum + data_list[j]
+
+        new_mean_list.append( sum / counter)
+
+
+    return new_mean_list
 
 def kmeans(k: int) -> None:
     means_x = [] #length
@@ -410,7 +367,7 @@ def kmeans(k: int) -> None:
         means_y.append(petal_width[rand])
 
     #TODO PLOT These points
-
+    plot_with_mean_points(means_x, means_y, "Initial Means")
     #Calculate the distance between each data point and cluster centers.
 
     distances_from_clusters = {}
@@ -449,21 +406,69 @@ def kmeans(k: int) -> None:
 
         cluster_assign.appned((index)) #0, 1, 2
 
-
     #all things assigned to cluster 1 will be in one calcualtion
 
+    new_cluster_assign = [0] * len(cluster_assign)
+    old_cluster_assign = cluster_assign
+    count = 0
+    while(check_if_need_to_loop(new_cluster_assign, old_cluster_assign)):
+        print(count)
+        count++
 
-    #estimate (update) means
+        old_cluster_assign = new_cluster_assign
+
+        #recalcualte new clusters (update x mean and y_mean)
+        means_x = update_cluster_mean(means_x, petal_length, new_cluster_assign)
+        means_y = update_cluster_mean(means_y, petal_width, new_cluster_assign)
+
+        #
+        dist_from_clusters = {}
+
+        #calcualte distance from clusters all other points
+        for i in range(0, k):
+            distance_list = []
+            for j in range(0,len(petal_length)):
+                x1 = petal_length[j]
+                x2 = means_x[i]
+
+                y1 = petal_width[j]
+                y2 = means_y[i]
+
+                distance_list.append(distance(x1, y1, x2 ,y2))
+
+            dist_from_clusters[i] = distance_list
+
+        #assign data points to new clusters
+        for i in range(0, len(petal_length)):
+            cluster_distances_for_i = []
+
+            #get the distances for this point
+            for key in distances_from_clusters:
+                dist_list = distances_from_clusters[key]
+                cluster_distances_for_i.append( dist_list[i] )
+
+            index = 0
+            min = cluster_distances_for_i[0]
+
+            for j in range(0, len(cluster_distances_for_i)):
+                if cluster_distances_for_i[j] < min:
+                    index = j
 
 
-    #repeat until converged
+            new_cluster_assign.appned((index)) #0, 1, 2
+
+
+    plot_with_mean_points(means_x, means_y, "Final Means")
+
+
 
 def extra_credit() -> None:
     kmeans(2)
-    kmeans(3)
+    #kmeans(3)
 
 ##################################################################################################################
 #hw5_p1_partc()
 #hw5_p2_parta()
 #hw5_p2_partc()
-get_data_from_file()
+#get_data_from_file()
+extra_credit()
